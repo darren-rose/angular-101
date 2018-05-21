df1ec07ae02b5ea972a13210a2dac88c47ae61e5
darren-rose <darrenwrose@gmail.com>
2018-05-21 18:54:44 +0200

    wip

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,12 +16,12 @@ describe('AppComponent', () => {
   it(`should have as title 'world'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('world');
+    expect(app.title).toEqual('Tour Of Heroes');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Hello world');
+    expect(compiled.querySelector('h1').textContent).toContain('Tour Of Heroes');
   }));
 });
